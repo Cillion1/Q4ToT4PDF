f1a7f892be5b353c8cd78ec47a6953c70aa36e28
terrencekuang <terrence.kuang@ucalgary.ca>
2022-06-01 12:07:38 -0600

    add report function

--- InfoProcessor.cs
+++ InfoProcessor.cs
@@ -74,6 +74,102 @@ namespace QBToT4PDF
             pdfDocument.Close();
         }
 
+        public static PayrollSumReport getPayrollSumAttribute(string response)
+        {
+            //Console.WriteLine(response + "\n");
+
+            XmlDocument inputXMLDoc = CreateXmlHeaders();
+            XmlElement qbXMLMsgsRq = (XmlElement)inputXMLDoc.GetElementsByTagName("QBXMLMsgsRq")[0];
+
+            XmlElement reportRq = inputXMLDoc.CreateElement("PayrollSummaryReportQueryRq");
+            qbXMLMsgsRq.AppendChild(reportRq);
+
+            reportRq.AppendChild(inputXMLDoc.CreateElement("PayrollSummaryReportType")).InnerText = "PayrollSummary";
+            reportRq.AppendChild(inputXMLDoc.CreateElement("DisplayReport")).InnerText = "false";
+
+
+            XmlElement dateRange = inputXMLDoc.CreateElement("ReportPeriod");
+            reportRq.AppendChild(dateRange);
+
+            dateRange.AppendChild(inputXMLDoc.CreateElement("FromReportDate")).InnerText = "2021-01-01";
+            dateRange.AppendChild(inputXMLDoc.CreateElement("ToReportDate")).InnerText = "2021-12-31";
+
+
+            reportRq.AppendChild(inputXMLDoc.CreateElement("SummarizeColumnsBy")).InnerText = "TotalOnly";
+
+
+
+            string input = inputXMLDoc.OuterXml;
+
+            string response = SetupConnection(input);
+
+
+            XmlDocument outputXMLDoc = new XmlDocument();
+            outputXMLDoc.LoadXml(response);
+
+            /*            XmlNodeList qbXMLMsgsRsNodeList = outputXMLDoc.GetElementsByTagName("PayrollSummaryReportQueryRs");
+                        XmlNodeList PayrollSummaryReportQueryRs = qbXMLMsgsRsNodeList.Item(0).ChildNodes;
+                        XmlNodeList ReportRet = PayrollSummaryReportQueryRs.Item(0).ChildNodes;*/
+            XmlNodeList qbXMLMsgsRsNodeList = outputXMLDoc.GetElementsByTagName("ReportData");
+            XmlNodeList ReportData = qbXMLMsgsRsNodeList.Item(0).ChildNodes;
+            XmlNodeList qbXMLMsgsRsNodeList2 = outputXMLDoc.GetElementsByTagName("ReportRet");
+            XmlNodeList ReportRet = qbXMLMsgsRsNodeList2.Item(0).ChildNodes;
+
+            XmlNodeList qbXMLMsgsRsNodeList3 = outputXMLDoc.GetElementsByTagName("DataRow");
+            XmlNodeList DataRow = qbXMLMsgsRsNodeList3.Item(0).ChildNodes;
+            XmlNodeList qbXMLMsgsRsNodeList4 = outputXMLDoc.GetElementsByTagName("SubtotalRow");
+            XmlNodeList SubtotalRow = qbXMLMsgsRsNodeList4.Item(0).ChildNodes;
+
+            string rowNumber = "";
+            PayrollSumReport report = new PayrollSumReport();
+
+
+            foreach (XmlNode node in ReportData)
+            {
+                if (node.Name.Equals("SubtotalRow"))
+                {
+                    foreach (XmlNode InnerNode in node)
+                    {
+                        if (InnerNode.Attributes["colID"].Value.Equals("1"))
+                        {
+                            if (InnerNode.Attributes["value"].Value.Equals("Total Gross Pay"))
+                            {
+                                report.employmentIncome = InnerNode.NextSibling.Attributes["value"].Value;
+                            }
+                        }
+                    }
+                }
+                else if (node.Name.Equals("DataRow"))
+                {
+                    foreach (XmlNode InnerNode in node)
+                    {
+                        if (InnerNode.Attributes["value"].Value.Equals("Federal Income Tax"))
+                        {
+                            report.incomeTaxDeducted = InnerNode.NextSibling.Attributes["value"].Value;
+                        }
+                        else if (InnerNode.Attributes["value"].Value.Equals("CPP - Employee"))
+                        {
+                            report.employeeCPPContribution = InnerNode.NextSibling.Attributes["value"].Value;
+                        }
+                        else if (InnerNode.Attributes["value"].Value.Equals("EI - Employee"))
+                        {
+                            report.employeeEIPremium = InnerNode.NextSibling.Attributes["value"].Value;
+                        }
+                        else if (InnerNode.Attributes["value"].Value.Equals("CPP - Company"))
+                        {
+                            report.employerCPPContribution = InnerNode.NextSibling.Attributes["value"].Value;
+                        }
+                        else if (InnerNode.Attributes["value"].Value.Equals("EI - Company"))
+                        {
+                            report.employerEIPremium = InnerNode.NextSibling.Attributes["value"].Value;
+                        }
+                    }
+                }
+            }
+            return report;
+        }
+
+
 
     }
 }
---
+++ PayrollSumReport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q4ToT4PDF
+{
+    public class PayrollSumReport
+    {
+        public string employmentIncome { get; set; }
+        public string employeeCPPContribution { get; set; }
+        public string employerCPPContribution { get; set; }
+        public string employeeEIPremium { get; set; }
+        public string employerEIPremium { get; set; }
+        public string incomeTaxDeducted { get; set; }
+        public string totalDeductionsReported { get; set; }
+    }
+}
